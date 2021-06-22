c0c4d8be6c7957962eaae3735b9b78caee8f5b9d
famhawite-infotech <86256491+famhawite-infotech@users.noreply.github.com>
2021-06-22 17:03:26 +0530

    Update global_kicks_sql.py

--- baby/modules/sql/global_kicks_sql.py
+++ baby/modules/sql/global_kicks_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, UnicodeText, Integer, String, Boolean
 
-from miley.modules.sql import BASE, SESSION
+from baby.modules.sql import BASE, SESSION
 
 class GloballyKickedUsers(BASE):
     __tablename__ = "gkicks"
