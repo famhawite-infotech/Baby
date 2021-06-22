8d96d60efa4470335cb515fe02278a62d85989d1
famhawite-infotech <86256491+famhawite-infotech@users.noreply.github.com>
2021-06-22 16:41:16 +0530

    Update rules.py

--- baby/modules/rules.py
+++ baby/modules/rules.py
@@ -6,10 +6,10 @@ from telegram.error import BadRequest
 from telegram.ext import CommandHandler, run_async, Filters
 from telegram.utils.helpers import escape_markdown
 
-import miley.modules.sql.rules_sql as sql
-from miley import dispatcher
-from miley.modules.helper_funcs.chat_status import user_admin
-from miley.modules.helper_funcs.string_handling import markdown_parser
+import baby.modules.sql.rules_sql as sql
+from baby import dispatcher
+from baby.modules.helper_funcs.chat_status import user_admin
+from baby.modules.helper_funcs.string_handling import markdown_parser
 
 
 @run_async
