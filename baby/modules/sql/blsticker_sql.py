d379560858f01088f734d57b43c8f310fc518f7e
famhawite-infotech <86256491+famhawite-infotech@users.noreply.github.com>
2021-06-22 16:59:05 +0530

    Update blsticker_sql.py

--- baby/modules/sql/blsticker_sql.py
+++ baby/modules/sql/blsticker_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import func, distinct, Column, String, UnicodeText, Integer
 
-from miley.modules.sql import SESSION, BASE
+from baby.modules.sql import SESSION, BASE
 
 
 class StickersFilters(BASE):
