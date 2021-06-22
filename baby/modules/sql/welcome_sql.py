48269a3a14beba0f206235e7e9553dca4929ce4b
famhawite-infotech <86256491+famhawite-infotech@users.noreply.github.com>
2021-06-22 17:09:47 +0530

    Update welcome_sql.py

--- baby/modules/sql/welcome_sql.py
+++ baby/modules/sql/welcome_sql.py
@@ -2,8 +2,8 @@ import threading
 
 from sqlalchemy import Column, String, Boolean, UnicodeText, Integer, BigInteger
 
-from miley.modules.helper_funcs.msg_types import Types
-from miley.modules.sql import SESSION, BASE
+from baby.modules.helper_funcs.msg_types import Types
+from baby.modules.sql import SESSION, BASE
 
 DEFAULT_WELCOME = "Hey {first}, Welcome to {chatname}! How you doing?"
 DEFAULT_GOODBYE = "Nice knowing ya!"
