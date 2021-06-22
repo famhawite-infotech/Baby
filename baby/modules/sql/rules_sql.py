eb9258dfd435a5bdb42f44ec6727e0cb3587bc24
famhawite-infotech <86256491+famhawite-infotech@users.noreply.github.com>
2021-06-22 17:07:14 +0530

    Update rules_sql.py

--- baby/modules/sql/rules_sql.py
+++ baby/modules/sql/rules_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText, func, distinct
 
-from miley.modules.sql import SESSION, BASE
+from baby.modules.sql import SESSION, BASE
 
 
 class Rules(BASE):
