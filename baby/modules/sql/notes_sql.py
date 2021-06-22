dc2dddea80424429589629267bab01522aa7e2a9
famhawite-infotech <86256491+famhawite-infotech@users.noreply.github.com>
2021-06-22 17:05:58 +0530

    Update notes_sql.py

--- baby/modules/sql/notes_sql.py
+++ baby/modules/sql/notes_sql.py
@@ -3,8 +3,8 @@ import threading
 
 from sqlalchemy import Column, String, Boolean, UnicodeText, Integer, func, distinct
 
-from miley.modules.helper_funcs.msg_types import Types
-from miley.modules.sql import SESSION, BASE
+from baby.modules.helper_funcs.msg_types import Types
+from baby.modules.sql import SESSION, BASE
 
 
 class Notes(BASE):
