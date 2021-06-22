c17b3ea29c0a99cda4c89ae1b059f310510f71ca
famhawite-infotech <86256491+famhawite-infotech@users.noreply.github.com>
2021-06-22 16:59:54 +0530

    Update cleaner_sql.py

--- baby/modules/sql/cleaner_sql.py
+++ baby/modules/sql/cleaner_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText, Boolean
 
-from miley.modules.sql import BASE, SESSION
+from baby.modules.sql import BASE, SESSION
 
 
 class CleanerBlueTextChatSettings(BASE):
