03c13519d50c9a127ebb09945b208448480fbb3e
famhawite-infotech <86256491+famhawite-infotech@users.noreply.github.com>
2021-06-22 19:49:59 +0530

    Update system_info.py

--- baby/modules/system_info.py
+++ baby/modules/system_info.py
@@ -63,7 +63,7 @@ def status(bot: Bot, update: Update):
 	chat = update.effective_chat
 	
 	stat = "--- System Status ---\n"
-	stat += f"miley Version: `{VERSION}`""\n"
+	stat += f"baby Version: `{VERSION}`""\n"
 	stat += "Python Version: `"+python_version()+"`\n"
 	stat += "GitHub API Version: `"+str(git.vercheck())+"`\n"
 	#Software Info
