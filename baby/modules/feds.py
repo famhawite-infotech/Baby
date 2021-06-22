1af02f44d6b969f00c18baffe72d093395444e0e
famhawite-infotech <86256491+famhawite-infotech@users.noreply.github.com>
2021-06-22 20:20:58 +0530

    Update feds.py

--- baby/modules/feds.py
+++ baby/modules/feds.py
@@ -1053,7 +1053,7 @@ def fed_ban_list(bot: Bot, update: Update, args: List[str], chat_data):
                 backups += "{user_id},{first_name},{last_name},{user_name},{reason}".format(user_id=users, first_name=getuserinfo['first_name'], last_name=getuserinfo['last_name'], user_name=getuserinfo['user_name'], reason=getuserinfo['reason'])
                 backups += "\n"
             with BytesIO(str.encode(backups)) as output:
-                output.name = "miley_fbanned_users.csv"
+                output.name = "baby_fbanned_users.csv"
                 update.effective_message.reply_document(document=output, filename="miley_fbanned_users.csv",
                                                     caption="Total {} User are blocked by Federation {}.".format(len(getfban), info['fname']))
             return
