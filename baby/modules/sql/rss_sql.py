c60dbaab4219ac19e38b9fbc61d2ea3d371a7e95
famhawite-infotech <86256491+famhawite-infotech@users.noreply.github.com>
2021-06-22 17:06:44 +0530

    Update rss_sql.py

--- baby/modules/sql/rss_sql.py
+++ baby/modules/sql/rss_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, UnicodeText, Integer
 
-from miley.modules.sql import BASE, SESSION
+from baby.modules.sql import BASE, SESSION
 
 
 class RSS(BASE):
