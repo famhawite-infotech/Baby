90100439d76ec2426c01625f1d152e600954c3c1
famhawite-infotech <86256491+famhawite-infotech@users.noreply.github.com>
2021-06-22 17:09:13 +0530

    Update warns_sql.py

--- baby/modules/sql/warns_sql.py
+++ baby/modules/sql/warns_sql.py
@@ -3,7 +3,7 @@ import threading
 from sqlalchemy import Integer, Column, String, UnicodeText, func, distinct, Boolean
 from sqlalchemy.dialects import postgresql
 
-from miley.modules.sql import SESSION, BASE
+from baby.modules.sql import SESSION, BASE
 
 
 class Warns(BASE):
