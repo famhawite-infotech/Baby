888c8095b282f7d79982edf29216cbcb0eac36c9
famhawite-infotech <86256491+famhawite-infotech@users.noreply.github.com>
2021-06-22 16:37:20 +0530

    Update reactions.py

--- baby/modules/reactions.py
+++ baby/modules/reactions.py
@@ -3,8 +3,8 @@ import random
 from telegram import Bot, Update
 from telegram.ext import run_async
 
-from miley import dispatcher
-from miley.modules.disable import DisableAbleCommandHandler
+from baby import dispatcher
+from baby.modules.disable import DisableAbleCommandHandler
 
 
 reactions = [
