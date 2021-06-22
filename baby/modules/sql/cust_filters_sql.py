5f43a1699c77d7e37b46dd85b715d8e9bd5dd90b
famhawite-infotech <86256491+famhawite-infotech@users.noreply.github.com>
2021-06-22 17:01:03 +0530

    Update cust_filters_sql.py

--- baby/modules/sql/cust_filters_sql.py
+++ baby/modules/sql/cust_filters_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText, Boolean, Integer, distinct, func
 
-from miley.modules.sql import BASE, SESSION
+from baby.modules.sql import BASE, SESSION
 
 
 class CustomFilters(BASE):
