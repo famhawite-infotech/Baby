728b75e922fd9219759f6eb548c59c18af92fd0c
famhawite-infotech <86256491+famhawite-infotech@users.noreply.github.com>
2021-06-22 16:26:05 +0530

    Update global_mutes.py

--- baby/modules/global_mutes.py
+++ baby/modules/global_mutes.py
@@ -7,13 +7,13 @@ from telegram.error import BadRequest, TelegramError
 from telegram.ext import run_async, CommandHandler, MessageHandler, Filters
 from telegram.utils.helpers import mention_html
 
-import miley.modules.sql.global_mutes_sql as sql
-from miley import dispatcher, OWNER_ID, DEV_USERS, SUDO_USERS, SUPPORT_USERS, STRICT_GMUTE, GBAN_LOGS
-from miley.modules.helper_funcs.chat_status import user_admin, is_user_admin
-from miley.modules.helper_funcs.extraction import extract_user, extract_user_and_text
-from miley.modules.helper_funcs.filters import CustomFilters
-from miley.modules.helper_funcs.misc import send_to_list
-from miley.modules.sql.users_sql import get_all_chats
+import baby.modules.sql.global_mutes_sql as sql
+from baby import dispatcher, OWNER_ID, DEV_USERS, SUDO_USERS, SUPPORT_USERS, STRICT_GMUTE, GBAN_LOGS
+from baby.modules.helper_funcs.chat_status import user_admin, is_user_admin
+from baby.modules.helper_funcs.extraction import extract_user, extract_user_and_text
+from baby.modules.helper_funcs.filters import CustomFilters
+from baby.modules.helper_funcs.misc import send_to_list
+from baby.modules.sql.users_sql import get_all_chats
 
 GMUTE_ENFORCE_GROUP = 6
 
