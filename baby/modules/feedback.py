09b346f4feee34ce15546f071ed68e413bfd97d6
famhawite-infotech <86256491+famhawite-infotech@users.noreply.github.com>
2021-06-22 16:18:53 +0530

    Update feedback.py

--- baby/modules/feedback.py
+++ baby/modules/feedback.py
@@ -1,8 +1,8 @@
 import html
 from telegram import Update, Bot, ParseMode
 from telegram.ext import run_async
-from miley.modules.disable import DisableAbleCommandHandler
-from miley import dispatcher
+from baby.modules.disable import DisableAbleCommandHandler
+from baby import dispatcher
 from requests import get
 from telegram import ParseMode, InlineKeyboardMarkup, InlineKeyboardButton
 
