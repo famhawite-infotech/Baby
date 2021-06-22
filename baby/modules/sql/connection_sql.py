90ff3743c955330f2c717d56a1c1a8208c157bff
famhawite-infotech <86256491+famhawite-infotech@users.noreply.github.com>
2021-06-22 17:00:30 +0530

    Update connection_sql.py

--- baby/modules/sql/connection_sql.py
+++ baby/modules/sql/connection_sql.py
@@ -4,8 +4,8 @@ from typing import Union
 
 from sqlalchemy import Column, String, Boolean, UnicodeText, Integer, func, distinct
 
-from miley.modules.helper_funcs.msg_types import Types
-from miley.modules.sql import SESSION, BASE
+from baby.modules.helper_funcs.msg_types import Types
+from baby.modules.sql import SESSION, BASE
 
 class ChatAccessConnectionSettings(BASE):
     __tablename__ = "access_connection"
