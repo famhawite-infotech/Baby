98590aba561c75141724f6426fc1327e72e9cd5a
famhawite-infotech <86256491+famhawite-infotech@users.noreply.github.com>
2021-06-23 18:59:09 +0530

    Update gps.py

--- baby/modules/gps.py
+++ baby/modules/gps.py
@@ -14,7 +14,6 @@ from baby import dispatcher, OWNER_ID, SUDO_USERS, SUPPORT_USERS, WHITELIST_USER
 from baby.__main__ import STATS, USER_INFO
 from baby.modules.disable import DisableAbleCommandHandler
 from baby.modules.helper_funcs.extraction import extract_user
-from baby.modules.helper_funcs.filters import CustomFilters
 
 from geopy.geocoders import Nominatim
 from telegram import Location
