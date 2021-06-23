abdc4c723b568c9e2c418a1dc0e5a4292b4203d0
famhawite-infotech <86256491+famhawite-infotech@users.noreply.github.com>
2021-06-23 18:58:06 +0530

    Update global_kick.py

--- baby/modules/global_kick.py
+++ baby/modules/global_kick.py
@@ -7,7 +7,6 @@ from telegram.utils.helpers import mention_html
 from baby import dispatcher, OWNER_ID, SUDO_USERS, SUPPORT_USERS, GBAN_LOGS, DEV_USERS
 from baby.modules.helper_funcs.chat_status import user_admin, is_user_admin
 from baby.modules.helper_funcs.extraction import extract_user, extract_user_and_text
-from baby.modules.helper_funcs.filters import CustomFilters
 from baby.modules.helper_funcs.misc import send_to_list
 from baby.modules.sql.users_sql import get_all_chats
 import baby.modules.sql.global_kicks_sql as sql
